fcdfb29a2757c1a57d7c2c35afe2b0952fbe5ace
Soutern-1 <81514298+Soutern-1@users.noreply.github.com>
2025-05-23 16:59:55 +0200

    Add files via upload

---
+++ class7/facedetection.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+
+# Initialize the machine learning algorithm to what we want to detect. 
+
+facedetection = mp.solutions.face_detection
+
+# Initialize how we draw on the screen
+
+mp_drawing = mp.solutions.drawing_utils
+
+webcam = cv2.VideoCapture(0)
+
+while True:
+    status,frame = webcam.read()
+    if not status:
+        print("Error")
+        break
+    else:
+        rgbframe = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        # Media pipe uses RGB and not BGR 
+        face_points = facedetection.process(rgbframe)
+        if face_points:
+            for detected_landmark in face_points.detection:
+                mp_drawing.draw_detection(frame,detected_landmark)
+
+        cv2.imshow("Video",frame)
+        if cv2.waitKey(1) & 0xff == 27:
+            break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+# Change CD to frameworks
+# Delete file (3.9)
+# Install 3.11
+# Install opencv + mediapipe 
+
+
